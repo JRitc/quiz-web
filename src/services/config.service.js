0b0c389fbfe978ca90881df94310b9c18f93db81
Jake <jake.ritchie7@gmail.com>
2024-04-30 15:15:31 +0100

    changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quiz-api-qa.us-east-1.elasticbeanstalk.com/';
 
     return service;
 }]);
